f1370cf6fb7dfa3b258e7ff22b255a9dcb7d5dcb
Anuj Mathur <anuj.mathur@rewardle.com>
2021-04-26 10:07:47 +0000

    Converting jsx code to es2015

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import * as esbuild from "esbuild-wasm"
+import { useEffect, useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { unpkgPathPlugin } from "./plugin/unpkg-path-plugin";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    const ref:any = useRef()
+    const [code,setCode] = useState('')
+    const [input,setInput] = useState('')
+    const startService = async ()=>{
+        ref.current = await esbuild.startService({
+            worker:true,
+            wasmURL:"/esbuild.wasm"
+        });
+       
+    }
+    useEffect(()=>{
+        startService()
+    },[])
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    const onClickHandler = async()=>{
+        if(!ref.current){
+            return;
+        }
+//this is for transform the code.
+    //    const result = await ref.current.transform(input,{
+    //        loader:'jsx',
+    //        target:'es2015'
+    //    });
+
+    //this is for bundeled the code.
+    const result = await ref.current.build({
+        entryPoints:['index.js'],
+        bundle:true,
+        write:false,
+        plugins:[unpkgPathPlugin()]
+    })
+       setCode(result.outputFiles[0].text)
+    }
+    return (
+        <div>
+            <textarea value={input} onChange={(e)=>setInput(e.target.value)}></textarea>
+            <div>
+                <button onClick={onClickHandler}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />,document.querySelector("#root"))
